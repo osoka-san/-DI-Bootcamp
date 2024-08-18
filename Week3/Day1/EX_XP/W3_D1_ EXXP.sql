ba1d41b2d1094e8cb589e40ffe7ef1cc9a16350a
osoka-san <lanesezra@gmail.com>
2024-08-18 20:14:07 +0300

    Add files via upload

---
+++ Week3/Day1/EX_XP/W3_D1_ EXXP.sql
@@ -0,0 +1,6 @@
+SELECT * FROM items;
+SELECT * FROM items WHERE price > 80;
+SELECT * FROM items WHERE price <= 300;
+SELECT * FROM customers WHERE last_name LIKE '%Smith';
+SELECT * FROM customers WHERE last_name LIKE '%Jones';
+SELECT * FROM customers WHERE first_name NOT LIKE 'Scott%';
