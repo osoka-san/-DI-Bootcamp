9b1cf85928482cd1b69f3bf2749f8e02420d5c1b
osoka-san <lanesezra@gmail.com>
2024-08-18 20:50:47 +0300

    Update W3_D1_EXXP+.sql

--- Week3/Day1/EX_XP/W3_D1_EXXP+.sql
+++ Week3/Day1/EX_XP/W3_D1_EXXP+.sql
@@ -1,10 +1,10 @@
 SELECT first_name, last_name FROM students;
-SELECT first_name, last_name FROM students WHERE id = 2;
-SELECT first_name, last_name FROM students WHERE last_name = 'Benichou' AND first_name = 'Marc';
-SELECT first_name, last_name FROM students WHERE last_name = 'Benichou' OR first_name = 'Marc';
-SELECT first_name, last_name FROM students WHERE first_name LIKE '%a%';
-SELECT first_name, last_name FROM students WHERE first_name LIKE 'a%';
-SELECT first_name, last_name FROM students WHERE first_name LIKE 'a%';
-SELECT first_name, last_name FROM students WHERE first_name LIKE '%a_';
-SELECT first_name, last_name FROM students WHERE id IN (1, 3);
+SELECT * FROM students WHERE id = 2;
+SELECT * FROM students WHERE last_name = 'Benichou' AND first_name = 'Marc';
+SELECT * FROM students WHERE last_name = 'Benichou' OR first_name = 'Marc';
+SELECT * FROM students WHERE first_name LIKE '%a%';
+SELECT * FROM students WHERE first_name LIKE 'a%';
+SELECT * FROM students WHERE first_name LIKE 'a%';
+SELECT * FROM students WHERE first_name LIKE '%a_';
+SELECT * FROM students WHERE id IN (1, 3);
 SELECT * FROM students WHERE birth_date >= '2000-01-01';
